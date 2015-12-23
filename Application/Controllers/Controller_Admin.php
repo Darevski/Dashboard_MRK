b97217f7c36010f51f28a8d1b17685f319cd18bc
Victor Darevski <mr.darevski@gmail.com>
2015-12-23 15:51:55 +0300

    BackEnd Отображение стартовой страницы, согласно привилегии текущего пользователя.

--- Application/Controllers/Controller_Admin.php
+++ Application/Controllers/Controller_Admin.php
@@ -19,25 +19,15 @@ class Controller_Admin extends Core\Controller{
     }
 
     /**
-     * Проверка на соответствие пользователя следующим критериям
-     * 1) Пользователь входил в систему. В сессиях весит хэш и логин
-     * 2) Хэш совпадает с хэшом в бд
-     * 3) Пользователь имеет необходимые привелегии
+     * При наличии у пользователя модификатора доступа пользователя пропускает дальше
      * В противном случае выбрасывается исключение
      * @throws UFO_Except
      */
     private function validate(){
-        if (isset($_SESSION['login']) & isset($_SESSION['hash'])){
-            $result=$this->auth_model->take_privilege($_SESSION['login'],$_SESSION['hash']);
-            if ($result == false) { // при не совпадении очистка сесии
-                $this->auth_model->clear_hash($_SESSION['login']);
-                throw new UFO_Except ('Доступ заблокирован, несовпадение контрольного хэша, перезайдите в систему', 601);
-            }
-            if ($result !== 'Admin')
-                throw new UFO_Except ('У вас не достаточно прав для просмотра данного контента', 403);
-        }
-        else
-            throw new UFO_Except ('Доступ запрещен в раздел запрещен не авторизированным пользоваталям, войдите в систему',401);
+        // Получение значения привелегии
+        $result = $this->state_authorization();
+        if ($result !== 'Admin')
+            throw new UFO_Except ('У вас не достаточно прав для просмотра данного контента', 403);
     }
     public function action_start(){
        echo 'Теперь вы диктуете правила!';
